bc86c6c816cac5c1d79922de9d35e7c189d5cceb
ksj <202033008@yiu.ac.kr>
2025-11-05 15:50:44 +0900

    imgtest

--- src/main/java/yongin/Yongnuri/_Campus/service/ImageService.java
+++ src/main/java/yongin/Yongnuri/_Campus/service/ImageService.java
@@ -1,6 +1,7 @@
 package yongin.Yongnuri._Campus.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,12 +14,14 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ImageService {
 
     @Value("${file.upload-dir}")
     private String uploadDir;
 
     public List<String> uploadImages(List<MultipartFile> imageFiles) {
+        log.info("Uploading images...");
         List<String> generatedUrls = new ArrayList<>();
 
         // ✅ 업로드 루트 디렉토리 보장
